3bba23b05f30acaea3a804111eceeb1441549d3b
Joern Kalz <joern.kalz@gmail.com>
2020-08-13 08:39:40 +0200

    Adds spring boot server

---
+++ server/src/main/java/com/github/joernkalz/openapispringangularstarter/OpenapiSpringAngularStarterApplication.java
@@ -0,0 +1,13 @@
+package com.github.joernkalz.openapispringangularstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapiSpringAngularStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapiSpringAngularStarterApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/github/joernkalz/openapispringangularstarter/OpenapiSpringAngularStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.joernkalz.openapispringangularstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiSpringAngularStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
